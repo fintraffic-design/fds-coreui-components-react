2cff700fb9c17cfb19ea9c5638aaee6787ea35e4
Kari Söderholm <kari.soderholm@solita.fi>
2024-02-01 09:04:48 +0200

    fix: Dropdown generics
    Create a workaround for keeping generic value type of Dropdown component when creating a React wrapper.
    
    Note: this React component is unusable until this bug is fixed in the web component repository https://github.com/fintraffic-design/fds-coreui-components/issues/65 but this change has been tested with a locally patched version of the web component.

--- src/dropdown.ts
+++ src/dropdown.ts
@@ -1,13 +1,13 @@
 import React from 'react'
-import { EventName, createComponent } from '@lit/react'
+import { EventName, ReactWebComponent, createComponent } from '@lit/react'
 import { FdsDropdown as _FdsDropdown, FdsDropdownEvent } from '@fintraffic/fds-coreui-components'
 import '@fintraffic/fds-coreui-components/dist/define/fds-dropdown'
 
 export { FdsDropdownEvent, FdsDropdownOption } from '@fintraffic/fds-coreui-components'
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const FdsDropdown = <T>() =>
-  createComponent({
+const FdsDropdownRaw = (function <T>() {
+  return createComponent({
     tagName: 'fds-dropdown',
     elementClass: _FdsDropdown<T>,
     react: React,
@@ -15,3 +15,26 @@ export const FdsDropdown = <T>() =>
       onSelect: 'select' as EventName<FdsDropdownEvent<T> & { target: _FdsDropdown<T> }>,
     },
   })
+})()
+
+// Workaround for creating a React component with generics so the component
+// user can specify the type argument `T` instead of it being `unknown`
+// Example usage in JSX with explic type argument:
+//   <FdsDropdown<string> ... />
+//   <FdsDropdown<number> ... />
+// TypeScript should also be able to infer the type from given `options` or
+// `value` property so this should work even without the type argument:
+//   <FdsDropdown ... />
+
+type FdsDropdownGeneric<T> = ReactWebComponent<
+  _FdsDropdown<T>,
+  {
+    onSelect: EventName<FdsDropdownEvent<T> & { target: _FdsDropdown<T> }>
+  }
+>
+type SourceProps<T> = React.ComponentProps<FdsDropdownGeneric<T>>
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-function-return-type
+export function FdsDropdown<T = any>(props: SourceProps<T>) {
+  return React.createElement(FdsDropdownRaw as FdsDropdownGeneric<T>, props)
+}
